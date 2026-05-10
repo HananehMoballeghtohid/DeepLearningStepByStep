502346b09d8b3069422ec759e1e7fa5961df0c82
hananeh.moballegh <hananeh.moballegh@tapsi.cab>
2026-05-10 17:01:26 +0330

    add simple data generation

---
+++ data_generation/simple_linear_regression.py
@@ -0,0 +1,15 @@
+
+data = pd.read_csv('housing.csv')
+low_price_houses = data[data['median_house_value'] < 500000].reset_index()
+X = low_price_houses["median_income"]
+y = low_price_houses['median_house_value']
+
+N = len(X)
+idx = np.arange(N)
+np.random.shuffle(idx)
+
+train_idx = idx[:int(N*0.8)]
+val_idx = idx[int(N*0.8):]
+
+X_train, y_train = X[train_idx].to_numpy().reshape(-1, 1), y[train_idx].to_numpy().reshape(-1, 1)
+X_val, y_val = X[val_idx].to_numpy().reshape(-1, 1), y[val_idx].to_numpy().reshape(-1, 1)
