b6e5e6da722a5609332e4dcbe654a12dc62e0e2b
hananeh.moballegh <hananeh.moballegh@tapsi.cab>
2026-05-10 17:00:50 +0330

    add version zero

---
+++ model_configuration/v0.py
@@ -0,0 +1,8 @@
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+torch.manual_seed(0)
+model = nn.Sequential(nn.Linear(1, 1)).to(device)
+
+optimizer = optim.SGD(model.parameters(), lr=lr)
+loss_function = nn.MSELoss(reduction='mean')
---
+++ model_training/v0.py
@@ -0,0 +1,12 @@
+
+epochs = 1000
+for epoch in range(epochs):
+    model.train()
+    
+    y_hat = model(X_train_tensor)
+    
+    loss = loss_function(y_hat, y_train_tensor)
+    loss.backward()
+    
+    optimizer.step()
+    optimizer.zero_grad()
