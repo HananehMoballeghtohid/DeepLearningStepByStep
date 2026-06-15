cc0bb55b7f12056c09baa1fa8aa92903a5839bf4
hananeh <hananeh.moballegh@tapsi.cab>
2026-06-15 13:19:08 +0330

    add tensorboard and save model

---
+++ model_configuration/v3.py
@@ -0,0 +1,21 @@
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+lr = 0.01
+
+torch.manual_seed(0)
+
+model = nn.Sequential(nn.Linear(1, 1)).to(device)
+
+loss_function = nn.MSELoss(reduction='mean')
+
+optimizer = optim.SGD(model.parameters(), lr=lr)
+
+# This will be used in the training loop
+train_step_function = make_train_step_fn(model, loss_function, optimizer)
+
+# This will be used for validation
+val_step_function = make_val_step_fn(model, loss_function)
+
+writer = SummaryWriter('runs/simple_linear_regression')
+x_dummy, y_dummy = next(iter(train_loader))
+writer.add_graph(model, x_dummy.to(device))
---
+++ model_training/v5.py
@@ -0,0 +1,28 @@
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+epochs = 1000
+
+# +
+losses = []
+val_losses = []
+
+for epoch in range(epochs):
+    if epoch % 100 == 0:
+        print(f'epoch {epoch}/{epochs}')
+        
+    loss = mini_batch_loop(train_loader, device, train_step_function)
+    losses.append(loss)
+    
+    with torch.no_grad():
+        val_loss = mini_batch_loop(val_loader, device, val_step_function)
+        val_losses.append(val_loss)
+
+    writer.add_scalars(
+        main_tag='loss',
+        tag_scalar_dict={
+            'training': loss,
+            'validation': val_loss
+            },
+        global_step=epoch
+    )
+
+writer.close()
